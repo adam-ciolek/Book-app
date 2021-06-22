8a828c4f8dfafe4d477f530cf2f92b4b63893561
Adam Ciołek <adasciolek@gmail.com>
2021-06-22 19:27:56 +0200

    Issues with method initActions

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,5 @@
 {
-  'use strict'
+  'use strict';
 
 
   const select = {
@@ -7,13 +7,22 @@
       bookProduct: '#template-book',
     },
     containerOf: {
-      bookList: '.book-list',
+      bookList: '.books-list',
+    },
+    booksList: {
+      bookImage: '.book__image',
+    },
+    filtersBooks: {
+      filters: '.filters',
     }
-  }
+  };
 
-
+  const links = {
+    favoriteImg: 'favorite'
+  };
   const template = Handlebars.compile(document.querySelector('#template-book').innerHTML);
-  // const booksList = document.querySelector('.book-list');
+  // const favoriteBooks = [];
+  // const filtersBooks = [];
 
 
   class Books {
@@ -21,12 +30,17 @@
 
       this.getElements();
       this.render();
+      this.initActions();
     }
 
 
     getElements() {
       this.bookProduct = document.querySelector(select.templateOf.bookProduct);
       this.bookList = document.querySelector(select.containerOf.bookList);
+      this.filters = document.querySelector(select.filtersBooks.filters);
+      // this.booksList = document.querySelectorAll(select.booksList.bookImage);
+      this.filtersArr = [];
+      this.favoriteBooks = [];
     }
 
     render() {
@@ -36,7 +50,7 @@
         const generatedHTML = template(book);
         // console.log(generatedHTML);
         const generateDom = utils.createDOMFromHTML(generatedHTML);
-
+        // console.log(generateDom);
         this.bookList.appendChild(generateDom);
 
       }
@@ -44,9 +58,89 @@
 
     }
 
+    initActions() {
+      // const favoriteBooks = [];
+      // const filters = [];
+
+      this.booksList = document.querySelectorAll(select.booksList.bookImage);
+      for (let img of this.booksList) {
+        img.addEventListener('dblclick', function (e) {
+          e.preventDefault();
+          const idImg = img.getAttribute('data-id');
+
+          // if (favoriteBooks.includes(idImg)) {
+          //   img.classList.remove('favorite');
+          //   const idImg = img.getAttribute('data-id');
+          //   const indexOf = favoriteBooks.indexOf(idImg);
+          //   favoriteBooks.splice(indexOf, 1);
+          // } else if (!(favoriteBooks === [])) {
+          //   img.classList.add('favorite');
+          //   console.log(idImg);
+          //   favoriteBooks.push(idImg);
+          //   console.log(favoriteBooks);
+          // }
+          if (e.target.offsetParent.classList.contains(links.favoriteImg)) {
+            // const idImg = img.getAttribute('data-id');
+            img.classList.remove(links.favoriteImg);
+            const indexOf = this.favoriteBooks.indexOf(idImg);
+            this.favoriteBooks.splice(indexOf, 1);
+            console.log(favoriteBooks);
+          } else if (!(this.favoriteBooks === [])) {
+            img.classList.add(links.favoriteImg);
+            console.log(idImg);
+            this.favoriteBooks.push(idImg);
+            console.log(this.favoriteBooks);
+          }
+        });
+      }
+
+      this.filters.addEventListener('click', function (e) {
+        if (e.target.tagName === 'INPUT' && e.target.type === 'checkbox' && e.target.name === 'filter') {
+          // console.log(e.target.value);
+        }
+
+        if (e.target.checked) {
+          this.filtersArr.push(e.target.value);
+          console.log(this.filtersArr);
+
+        } else if (!e.target.checked) {
+          const indexOf = this.filtersArr.indexOf(e.target.value);
+          this.filtersArr.splice(indexOf, 1);
+          console.log(this.filtersArr);
+        }
+
+      });
+      this.filterBooks();
+    }
+
+    filterBooks() {
+
+      for (let element of dataSource.books) {
+
+        let shouldBeHidden = false;
+
+        for (let filter of this.filtersArr) {
+          if (element.details[filter]);
+          shouldBeHidden = true;
+          break;
+        }
+        const bookId = element.id;
+        const selectedImg = document.querySelector('.book__image[data-id="' + bookId + '"]');
+
+
+        if (shouldBeHidden === true) {
+          selectedImg.classList.add('hidden');
+        }
+        else {
+          selectedImg.classList.remove('hidden');
+        }
+      }
+    }
   }
 
-  const book = new Books();
+
+
+  const newBook = new Books();
+  console.log(newBook);
 
 }
-
